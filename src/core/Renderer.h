22b5b99c57840c8b07ed72cd5057c914adeab5c4
Onirau <61100138+Onirau@users.noreply.github.com>
2026-01-21 02:45:23 +0000

    adssad

--- src/core/Renderer.h
+++ src/core/Renderer.h
@@ -2,20 +2,20 @@
 #include <vector>
 #include "../datatypes/Vector3.h"
 #include "../core/WindowManager.h"
+#include <glm/glm.hpp>
 
 // Forward declarations
 struct BasePart;
 
-// External global texture
+// External global texture - changed to pointer to match implementation
 namespace Engine { namespace Graphics { class Texture2D; } }
-extern Engine::Graphics::Texture2D g_defaultTexture;
+extern Engine::Graphics::Texture2D* g_defaultTexture;
 
-// Function declarations
-Engine::Graphics::Texture2D GenerateDefaultTexture(int width = 128, int height = 128);
+// Function declarations - corrected return types
+void GenerateDefaultTexture(int width = 128, int height = 128);
 
-// Matrix and lighting functions
-struct Matrix; // Forward declare Matrix type
-Matrix GetLightSpaceMatrix(const Vector3 &lightDir, const Vector3 &sceneCenter);
+// Matrix and lighting functions - use glm::mat4 instead of undefined Matrix
+glm::mat4 GetLightSpaceMatrix(const Vector3 &lightDir, const Vector3 &sceneCenter);
 
 void RenderShadowMap(const std::vector<BasePart *> &instances,
                      const Vector3 &lightDir, const Vector3 &sceneCenter);
