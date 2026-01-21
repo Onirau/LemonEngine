981eedaa0b2e22de270808bee020dcff2d0140c9
Onirau <61100138+Onirau@users.noreply.github.com>
2026-01-21 02:33:36 +0000

    fu

--- src/runtime/RuntimeApplication.h
+++ src/runtime/RuntimeApplication.h
@@ -8,11 +8,11 @@ private:
 
 protected:
     void RenderUI() override {
-        DrawText("WASD to move camera, Right Click to look around", 10, 10, 20,
-                 LIGHTGRAY);
-        DrawText("Hold LSHIFT to move slower", 10, 40, 20, LIGHTGRAY);
-        DrawText(TextFormat("FPS: %d", GetFPS()), 10, GetScreenHeight() - 30,
-                 20, GREEN);
+        // DrawText("WASD to move camera, Right Click to look around", 10, 10, 20,
+        //          LIGHTGRAY);
+        // DrawText("Hold LSHIFT to move slower", 10, 40, 20, LIGHTGRAY);
+        // DrawText(TextFormat("FPS: %d", GetFPS()), 10, GetScreenHeight() - 30,
+        //          20, GREEN);
     }
 
     void Initialize() override {
@@ -22,7 +22,7 @@ protected:
         // temp
         Part *baseplate = new Part();
 
-        baseplate->Color = Color3(Color{92, 92, 92, 0});
+        baseplate->Color = Color3(92, 92, 92);
         baseplate->Position = Vector3{0, -8, 0};
         baseplate->Size = Vector3{2048, 16, 2048};
         baseplate->Name = "Baseplate";
